6fc11a9972e6670f515854cc2a93c0605835b30d
Grong <lionel.de-sousa@epitech.eu>
2022-02-08 12:58:02 +0100

    v1

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+}
